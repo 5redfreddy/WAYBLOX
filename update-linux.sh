d1790bafbe5ceb17c5888138c9699e862a14f10d
quizcas <quizcaspersonal@gmail.com>
2025-07-26 14:33:20 -0400

    Update update-linux.sh

--- update-linux.sh
+++ update-linux.sh
@@ -1,3 +1,3 @@
-    #!/bin/bash
-    git pull
-    zenity --info --title="WAYBLOX" --text="WAYBLOX has successfully been updated or is already up-to-date!"
+#!/bin/bash
+git pull
+zenity --info --title="WAYBLOX" --text="WAYBLOX has successfully been updated or is already up-to-date!"
